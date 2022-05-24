6666f1783f2d92095794a12fff16e2e431e6fbae
EnricoLavoro <100861638+EnricoLavoro@users.noreply.github.com>
2022-05-24 16:26:54 +0200

    Update ITsp_TracProPN.sql

--- ITsp_TracProPN.sql
+++ ITsp_TracProPN.sql
@@ -1,6 +1,6 @@
 USE [up_marchioricontino]
 GO
-/****** Object:  StoredProcedure [dbo].[ITsp_TracProPN]    Script Date: 20/05/2022 17:52:07 ******/
+/****** Object:  StoredProcedure [dbo].[ITsp_TracProPN]    Script Date: 24/05/2022 16:16:34 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -17,13 +17,16 @@ BEGIN
 	SET NOCOUNT ON;
 
 	IF OBJECT_ID('tempdb..#TabContratti')	IS NOT NULL DROP TABLE #TabContratti;
+	IF OBJECT_ID('tempdb..#TabCompensi')	IS NOT NULL DROP TABLE #TabCompensi;
 	IF OBJECT_ID('tempdb..##OutputResult')	IS NOT NULL DROP TABLE ##OutputResult;
+	
+	-- =======================================================================================================================================
 
-	SELECT DISTINCT MbaisCSer, MbaisCMbais, MbaisCRAsog, TacaCRAsog, T.Attr1, T.Val1, T.Attr2, T.Val2
+	SELECT DISTINCT MbaisCMbais, MbaisCRTaca, MbaisTrichiesta, T.Attr1, T.Val1, T.Attr2, T.Val2, T.Attr3, T.Val3
 	INTO #TabContratti
 	FROM IstBaseStOpMaster
 	INNER JOIN TabAttComm ON MbaisCRTaca = TacaCser
-	INNER JOIN (SELECT T1.MbaisCSer AS Serial, T1.TgrsCTgrs AS Attr1, T1.RgrsCRgrs AS Val1, T2.TgrsCTgrs AS Attr2, T2.RgrsCRgrs AS Val2
+	INNER JOIN (SELECT T1.MbaisCSer AS Serial, T1.TgrsCTgrs AS Attr1, T1.RgrsCRgrs AS Val1, T2.TgrsCTgrs AS Attr2, T2.RgrsCRgrs AS Val2, T3.AttrCAttr AS Attr3, T3.KatisNvalatt AS Val3
 		FROM
 		(SELECT MbaisCSer, TgrsCTgrs, RgrsCRgrs
 		FROM IstBaseStopMaster
@@ -37,30 +40,44 @@ BEGIN
 		RIGHT JOIN TabGruppiStatistici AS TGRS1 ON TGRS1.TgrsCSer = KGSIS1.KgsisCRgrs
 		LEFT JOIN GruppiStatisticiRighe AS RGRS1 ON RGRS1.RgrsCSer = KGSIS1.KgsisCRvgrs) T2
 		ON T1.MbaisCSer = T2.MbaisCSer
-		WHERE T1.TgrsCTgrs='CTISA' AND T2.TgrsCTgrs='TUISA') T ON MbaisCSer = T.Serial
+		INNER JOIN 
+		(SELECT MbaisCSer, KatisNvalatt, AttrCAttr
+		FROM IstBaseStOpMaster INNER JOIN IstAttrStOpMColl ON KatisCRMbais = MbaisCSer
+		INNER JOIN Attributi ON AttrCSer = KatisCRattr) T3
+		ON T2.MbaisCSer = T3.MbaisCSer
+		WHERE T1.TgrsCTgrs='CTISA' AND T2.TgrsCTgrs='TUISA' AND T3.AttrCAttr = 'VAISA') T ON MbaisCSer = T.Serial
 	WHERE MbaisCRMcso=23;
 
-	---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- =======================================================================================================================================
 
 	SELECT ROW_NUMBER() OVER(ORDER BY M.MbaisCSer ASC) AS Progressivo,
 	NULL AS Nullo,
 	LEFT(TcsoCTcso,1) AS CausaleDocumento, 
 	'P_'+RIGHT(M.MbaisCMbais,13) AS NumeroDocumento,
-	M.MbaisTins AS DataEsportazione,
+	CONVERT(VARCHAR(10), M.MbaisTins, 111) AS DataEsportazione,
 	NULL AS SerieDocumento,
 	'SEDEP' AS SedeEmissione,
 	R2.RgrsCrgrs AS TipoSoggetto,
-	SogControparte.AsogCSer AS CodiceCliente,
-	R2.RgrsDrgrs AS DescrizioneCliente,
-	SogClienti.AsogCSer AS CodiceControparte,
-	R1.RgrsDrgrs AS DescrizioneControparte,
+	SogControparte.AsogCAsog AS CodiceCliente,
+	SogControparte.AsogDAsog AS DescrizioneCliente,
+	SogClienti.AsogCAsog AS CodiceControparte,
+	SogClienti.AsogDAsog AS DescrizioneControparte,
 	TabC.MbaisCMbais AS RiferimentoContratto,
 	NULL AS AnnotazioneTestata,
-	MbaisCRMmoc AS CodicePagamento,
-	SUBSTRING(SogClienti.AsogDatIBANBE,5,5) AS CodiceABI,
-	SUBSTRING(SogClienti.AsogDatIBANBE,10,5) AS CodiceCAB,
-	SogClienti.AsogDatIBANBE AS CodiceIBAN,
-	TabAttr.KatisNvalatt AS ValoreAffittoISA,
+	TmpaCTmpa AS CodicePagamento,
+	CASE WHEN SUBSTRING(SogClienti.AsogDatIBANBE,5,5) IS NULL OR SUBSTRING(SogClienti.AsogDatIBANBE,5,5) = ''
+	THEN NULL
+	ELSE SUBSTRING(SogClienti.AsogDatIBANBE,5,5)
+	END AS CodiceABI,
+	CASE WHEN SUBSTRING(SogClienti.AsogDatIBANBE,10,5) IS NULL OR SUBSTRING(SogClienti.AsogDatIBANBE,10,5) = ''
+	THEN NULL
+	ELSE SUBSTRING(SogClienti.AsogDatIBANBE,10,5)
+	END AS CodiceCAB,
+	CASE WHEN SogClienti.AsogDatIBANBE IS NULL OR SogClienti.AsogDatIBANBE = ''
+	THEN NULL
+	ELSE SogClienti.AsogDatIBANBE
+	END AS CodiceIBAN,
+	TabC.Val3 AS ValoreAffittoISA,
 	--CASE WHEN TabC.CodArtxPlus = 'CAN'
 	--	THEN TabC.PrezzoUnitario
 	--	ELSE (SELECT PrezzoUnitario FROM #TabContratti WHERE #TabContratti.MbaisCRAsog = SogClienti.AsogCSer AND #TabContratti.TacaCRAsog = SogControparte.AsogCSer AND #TabContratti.CodArtxPlus = 'CAN') 
@@ -97,7 +114,10 @@ BEGIN
 	0 AS Sconto2,
 	'+' AS SegnoMovimento,
 	YEAR(MbaisTins) AS APerCompetenza,
-	'M' AS TPerCompetenza,
+	'M' + CASE WHEN MONTH(M.MbaisTins) < 10
+	THEN '0' + CONVERT(VARCHAR(2),MONTH(M.MbaisTins))
+	ELSE CONVERT(VARCHAR(2),MONTH(M.MbaisTins))
+	END + ' ' + UPPER(SUBSTRING(DATENAME(month,M.MbaisTins),1,1)) + SUBSTRING(DATENAME(month,M.MbaisTins),2,LEN(DATENAME(month,M.MbaisTins))-1) AS TPerCompetenza,
 	12 AS NPerCompetenza,
 	TaliCTali AS CodIVAxPlus,
 	AscoCAsco AS CodContPartxPlus,
@@ -110,8 +130,7 @@ BEGIN
 	NULL AS UtenteUltMod,
 	NULL AS DataUltMod,
 	NULL AS Vuoto3,
-	NULL AS Vuoto4,
-	RbaisQoum * HCAISCRprzQImpValLoc * T2.Compenso / 100 AS Compenso
+	NULL AS Vuoto4
 	INTO ##OutputResult
 	FROM IstBaseStOpMaster AS M
 	INNER JOIN IstBaseStOpRighe ON MbaisCSer = RbaisCRMbais
@@ -123,6 +142,8 @@ BEGIN
 	INNER JOIN TabAttComm ON MbaisCRTaca = TacaCser
 	INNER JOIN ArticoliBase ON ArtbCSer = RbaisCRArtb
 
+	INNER JOIN IstCComAtStOpMColl ON M.MbaisCSer = KCAISCser
+	INNER JOIN TabModPagInc ON KCAISCRTmpi=TmpaCSer
 	INNER JOIN TabCausaliStOp ON M.MbaisCRTcso = TcsoCSer
 	INNER JOIN TabAttComm AS TabAtt  ON M.MbaisCRTaca = TabAtt.TacaCSer
 	INNER JOIN IstGrStatStOpMColl    ON KgsisCRMbais = MbaisCSer
@@ -146,26 +167,103 @@ BEGIN
 		INNER JOIN GruppiStatisticiRighe ON RgrsCSer = LsgsCRifVgrs
 		WHERE TgrsCTgrs='SOGRIT') AS T1 ON SogControparte.AsogCSer = T1.AsogCSer
 
-	LEFT JOIN (SELECT AsogCSer, RgrsCRgrs AS Compenso 
-		FROM 
-		Soggetti INNER JOIN LegSoggettiGruppiStat ON AsogCSer = LsgsCRifAsog
-		INNER JOIN TabGruppiStatistici ON TgrsCSer = LsgsCRifGrs
-		INNER JOIN GruppiStatisticiRighe ON RgrsCSer = LsgsCRifVgrs
-		WHERE TgrsCTgrs='COM') AS T2 ON SogControparte.AsogCSer = T2.AsogCSer
-
-	INNER JOIN #TabContratti AS TabC ON TabC.MbaisCRAsog = SogClienti.AsogCSer AND TabC.TacaCRAsog = SogControparte.AsogCSer
-
-	INNER JOIN (SELECT MbaisCSer, KatisNvalatt FROM IstBaseStOpMaster
-		INNER JOIN IstAttrStOpMColl ON KatisCRMbais = MbaisCSer
-		INNER JOIN Attributi ON AttrCSer = KatisCRattr
-		WHERE AttrCAttr = 'VAISA') AS TabAttr ON TabAttr.MbaisCSer = TabC.MbaisCSer
+	INNER JOIN #TabContratti AS TabC ON TabC.MbaisCRTaca = M.MbaisCRTaca AND M.MbaisTrichiesta = TabC.MbaisTrichiesta
 
 	WHERE MbaisCRMcso IN (25,29,31,33) AND TG0.TgrsCTgrs = 'EXPLUS' AND R0.RgrsCRgrs = 'S' AND TG1.TgrsCTgrs='TSO' AND TG2.TgrsCTgrs='TSO'
 	AND M.MbaisTins BETWEEN @parDaData AND @parAData
 	ORDER BY 1 ASC, SogControparte.AsogCSer ASC;
 
+	-- =======================================================================================================================================
+
 	SELECT * FROM #TabContratti;
 	SELECT * FROM ##OutputResult;
+
+	-- =======================================================================================================================================
+
+	SELECT ROW_NUMBER() OVER(ORDER BY CodiceCliente ASC) AS Prog, CodiceCliente, Compenso, MaxVal.MaxProg, SUM(ValoreAffittoISA) * Compenso / 100 AS ValoreCompenso
+	INTO #TabCompensi
+	FROM ##OutputResult
+	LEFT JOIN (SELECT AsogCAsog, RgrsCRgrs AS Compenso
+		FROM 
+		Soggetti INNER JOIN LegSoggettiGruppiStat ON AsogCSer = LsgsCRifAsog
+		INNER JOIN TabGruppiStatistici ON TgrsCSer = LsgsCRifGrs
+		INNER JOIN GruppiStatisticiRighe ON RgrsCSer = LsgsCRifVgrs
+		WHERE TgrsCTgrs='COM') AS T ON CodiceCliente = T.AsogCAsog
+	INNER JOIN (SELECT CodiceCliente CodC, MAX(Progressivo) MaxProg FROM ##OutputResult GROUP BY CodiceCliente) MaxVal ON MaxVal.CodC = CodiceCliente
+	WHERE CodArtxPlus='CAN'
+	GROUP BY CodiceCliente, Compenso, MaxVal.MaxProg;
+
+	-- =======================================================================================================================================
+
+	SELECT * FROM #TabCompensi;
+	DECLARE @numCompensi INT = @@ROWCOUNT;
+		
+	DECLARE @cnt INT = 1;
+	WHILE @cnt <= @numCompensi
+	BEGIN
+
+	IF OBJECT_ID('tempdb..#TabInsert')	IS NOT NULL DROP TABLE #TabInsert;
+
+	SELECT Progressivo+1 AS NuovoProg, * 
+	INTO #TabInsert
+	FROM ##OutputResult T1 
+	WHERE Progressivo=(SELECT TOP(1) MaxProg FROM #TabCompensi T1 INNER JOIN ##OutputResult T2 ON T1.CodiceCliente=T2.CodiceCliente WHERE Prog=@cnt);
+	
+	ALTER TABLE #TabInsert DROP COLUMN Progressivo;
+	INSERT INTO ##OutputResult SELECT * FROM #TabInsert;
+
+	UPDATE ##OutputResult SET
+	CausaleDocumento='C',
+	NumeroDocumento='COMP_'+(SELECT TOP(1) T1.CodiceCliente FROM #TabCompensi T1 INNER JOIN ##OutputResult T2 ON T1.CodiceCliente=T2.CodiceCliente WHERE Prog=@cnt),
+	DataEsportazione=CONVERT(VARCHAR(10), @parAData, 111),
+	-- SedeEmissione='SEDEP',
+	TipoSoggetto='C',
+	CodiceControparte=NULL,
+	DescrizioneControparte='INQUILINO',
+	RiferimentoContratto=NULL,
+	CodicePagamento=NULL,
+	CodiceABI=NULL,CodiceCAB=NULL,CodiceIBAN=NULL,
+	ValoreAffittoISA=0,
+	ConteggiaTrattativaISA=NULL,TrattativaUnilateraleISA=NULL,
+	NumeroRiga=CASE WHEN (SELECT MAX(MaxProg)+1 FROM #TabCompensi WHERE (MaxProg+1)<Progressivo) <> Progressivo
+	THEN Progressivo-(SELECT MAX(MaxProg)+1 FROM #TabCompensi WHERE (MaxProg+1)<Progressivo)+1
+	ELSE Progressivo END,
+	CodArtxPlus='CAN',
+	Desc1='CANONE',
+	Desc2=NULL,
+	PrezzoUnitario=(SELECT TOP(1) ValoreCompenso FROM #TabCompensi T1 INNER JOIN ##OutputResult T2 ON T1.CodiceCliente=T2.CodiceCliente WHERE Prog=@cnt),
+	APerCompetenza=YEAR(@parAData),
+	TPerCompetenza='M' + CASE WHEN MONTH(@parAData) < 10
+	THEN '0' + CONVERT(VARCHAR(2),MONTH(@parAData))
+	ELSE CONVERT(VARCHAR(2),MONTH(@parAData))
+	END + ' ' + UPPER(SUBSTRING(DATENAME(month,@parAData),1,1)) + SUBSTRING(DATENAME(month,@parAData),2,LEN(DATENAME(month,@parAData))-1),
+	-- CodIVAxPlus=NULL,
+	CodContPartxPlus=NULL,
+	SogRit=NULL
+	WHERE Progressivo=(SELECT TOP(1) MaxProg FROM #TabCompensi T1 INNER JOIN ##OutputResult T2 ON T1.CodiceCliente=T2.CodiceCliente WHERE Prog=@cnt)+1 AND CodiceCliente=(SELECT TOP(1) T1.CodiceCliente FROM #TabCompensi T1 INNER JOIN ##OutputResult T2 ON T1.CodiceCliente=T2.CodiceCliente WHERE Prog=@cnt);
+
+	IF OBJECT_ID('tempdb..#TabInsert')	IS NOT NULL DROP TABLE #TabInsert;
+	SET @cnt += 1
+	END
+
+	-- =======================================================================================================================================
+
+	IF OBJECT_ID('tempdb..#TabUpdate')	IS NOT NULL DROP TABLE #TabUpdate;
+	SELECT ROW_NUMBER() OVER (ORDER BY Progressivo ASC, NumeroDocumento ASC) AS Prog, *
+	INTO #TabUpdate
+	FROM  ##OutputResult;
+
+	ALTER TABLE #TabUpdate DROP COLUMN Progressivo;
+	DELETE FROM ##OutputResult;
+
+	INSERT INTO ##OutputResult
+	SELECT * FROM #TabUpdate;
+
+	-- =======================================================================================================================================
+
+	SELECT * FROM ##OutputResult
+
+	-- =======================================================================================================================================
 
 	DECLARE @command varchar(4000)
 	DECLARE @server_name varchar(200)
@@ -203,5 +301,7 @@ BEGIN
 	END
 
 	IF OBJECT_ID('tempdb..#TabContratti')	IS NOT NULL DROP TABLE #TabContratti;
+	IF OBJECT_ID('tempdb..#TabCompensi')	IS NOT NULL DROP TABLE #TabCompensi;
 	IF OBJECT_ID('tempdb..##OutputResult')	IS NOT NULL DROP TABLE ##OutputResult;
 END
+
