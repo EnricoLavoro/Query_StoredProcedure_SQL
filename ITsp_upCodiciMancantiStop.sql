b5cdfa780ea794faa72e4691fda58a481a741e92
EnricoLavoro <100861638+EnricoLavoro@users.noreply.github.com>
2022-09-22 17:50:05 +0200

    Update ITsp_upCodiciMancantiStop.sql

--- ITsp_upCodiciMancantiStop.sql
+++ ITsp_upCodiciMancantiStop.sql
@@ -1,26 +1,30 @@
 USE [up_Disenia]
 GO
 
-/****** Object:  StoredProcedure [dbo].[ITsp_upCodiciMancantiStop]    Script Date: 21/09/2022 17:18:54 ******/
+/****** Object:  StoredProcedure [dbo].[ITsp_upCodiciMancantiStop]    Script Date: 22/09/2022 17:47:31 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
- 
+
  /*
 	 Enrico - 21/09/2022 - CIT202200383 - CTRL BUCHI PROTOCOLLO
  */
 
-ALTER PROCEDURE [dbo].[ITsp_upCodiciMancantiStop]  
-						@Filtro varchar(2000),
-				  		@BatchJobId integer,
-				  		@MaxNumeroMancanti integer,
-						@numeroMancanti integer OUTPUT
+ALTER PROCEDURE [dbo].[ITsp_upCodiciMancantiStop]
+						@Filtro VARCHAR(2000),
+				  		@BatchJobId INTEGER,
+				  		@MaxNumeroMancanti INTEGER,
+						@numeroMancanti INTEGER OUTPUT
 AS
+-- Numero di iterazioni complessive del ciclo
 DECLARE @numIt INT
+-- Contatore del ciclo
 DECLARE @c INT = 1
+-- Progressivo di riga della tabella StampaCodiciMancantiStop
 DECLARE @Prig INT = 1
+-- Progressivo di confronto per verificare se ci sono buchi di protocollo
 DECLARE @prog INT
 DECLARE @maxIterazioni INT
 SET     @maxIterazioni = 40
@@ -29,11 +33,10 @@ SET		@numeroMancanti = 0
 IF @MaxNumeroMancanti > 40 and @MaxNumeroMancanti < 1000
 	SET     @maxIterazioni = @MaxNumeroMancanti
 
-
 IF OBJECT_ID('tempdb..#Tab')   IS NOT NULL DROP TABLE #Tab;
-
 DELETE FROM StampaCodiciMancantiStop WHERE scmstCserRepRif = @BatchJobId
 
+-- Creazione della tabella contenente tutti i buchi di protocollo individuati rispetto al filtro selezionato
 CREATE TABLE #Tab (NRiga INT, ProtSec VARCHAR(8000), ProgProtSec INT)
 EXEC('INSERT INTO #Tab SELECT ROW_NUMBER() OVER(ORDER BY CONVERT(INT,SUBSTRING(RIGHT(RpriCProRegIVA,9), PATINDEX(''%[1-9]%'',RIGHT(RpriCProRegIVA,9)), 10 - PATINDEX(''%[1-9]%'',RIGHT(RpriCProRegIVA,9)))) ASC) AS NRiga, LEFT(RIGHT(RpriCProRegIVA,13),4)+''/''+ SUBSTRING(RIGHT(RpriCProRegIVA,9), PATINDEX(''%[1-9]%'',RIGHT(RpriCProRegIVA,9)), 10 - PATINDEX(''%[1-9]%'',RIGHT(RpriCProRegIVA,9))) + ''/'' + TregCTreg AS ProtSec,
 CONVERT(INT,SUBSTRING(RIGHT(RpriCProRegIVA,9), PATINDEX(''%[1-9]%'',RIGHT(RpriCProRegIVA,9)), 10 - PATINDEX(''%[1-9]%'',RIGHT(RpriCProRegIVA,9)))) AS ProgProtSec
@@ -42,8 +45,7 @@ INNER JOIN TabCausaliStOp			  ON MbaisCRTcso = TcsoCSer
 INNER JOIN MasterMovContabile		  ON MbaisCRMmoc = MmocCSer
 INNER JOIN RigheProtocolliIVA		  ON RpriCRifMmoc = MmocCSer
 INNER JOIN TabRegistri			      ON RpriCRifTreg = TregCSer
-WHERE (MbaisCRMcso IN (4,5,23,50) OR TcsoDTcso LIKE ''%ADDEBITO%'') AND DATEDIFF(YEAR,MbaisTins,GETDATE())=2
-ORDER BY 3 ASC')
+WHERE TregCSer=6 AND ' + @Filtro + ' ORDER BY 3 ASC')
 
 --EXEC('INSERT INTO #Tab SELECT ROW_NUMBER() OVER(ORDER BY MbaisCprotsec ASC) AS NRiga, MbaisCprotsec AS ProtSec,
 --	   CONVERT(INT,RIGHT(MbaisCprotsec,9)) AS ProgProtSec
@@ -54,16 +56,18 @@ SELECT * FROM #Tab
 SET @numIt = @@ROWCOUNT
 SET @prog  = (SELECT ProgProtSec FROM #Tab WHERE NRiga = @c)
 
+-- Ciclando per il numero complessivo di buchi di protocollo trovati
 WHILE @c <= @numIt
 BEGIN
+	-- Se viene individuato un progressivo non in linea con quello previsto
 	IF @prog <> (SELECT ProgProtSec FROM #Tab WHERE NRiga = @c)
 	BEGIN
-		--SELECT 'Da ' + CONVERT(VARCHAR,(@prog - 1)) + ' a ' + CONVERT(VARCHAR,(SELECT ProgProtSec FROM #Tab WHERE NRiga = @c))
 		INSERT INTO StampaCodiciMancantiStop 
 					(scmstCserRepRif, scmstPrig, scmstCod1, scmstCod2, scmstDesc, scmstNmancanti, scmstFErrore) VALUES
-					(@BatchJobId, @Prig, (SELECT ProtSec FROM #Tab WHERE ProgProtSec = @prog-1), (SELECT ProtSec FROM #Tab WHERE NRiga = @c),'',(SELECT ProgProtSec FROM #Tab WHERE NRiga = @c)-@prog+2,'')
+					(@BatchJobId, @Prig, (SELECT ProtSec FROM #Tab WHERE ProgProtSec = @prog-1), 
+					(SELECT ProtSec FROM #Tab WHERE NRiga = @c),'',(SELECT ProgProtSec FROM #Tab WHERE NRiga = @c)-@prog,'')
 		
-		SET @numeroMancanti = @numeroMancanti + (SELECT ProgProtSec FROM #Tab WHERE NRiga = @c)-@prog+2
+		SET @numeroMancanti = @numeroMancanti + (SELECT ProgProtSec FROM #Tab WHERE NRiga = @c)-@prog
 		SET @prog = (SELECT ProgProtSec FROM #Tab WHERE NRiga = @c)
 		SET @Prig = @Prig + 1
 	END
@@ -73,5 +77,3 @@ END
 
 IF OBJECT_ID('tempdb..#Tab')   IS NOT NULL DROP TABLE #Tab;
 GO
-
-
